85110c491e8b0b5802e91bf93a165a448db2d899
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-22 17:03:12 -0400

    modificando el nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  /* basePath: '/dist', */ 
+  // Comenta la linea basePath para evitar el error 404
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source : '/hola',
+        destination: 'https://github.com/RudyErnesto', // IMPORTANTE, el link del destino sin / 
+        permanent: true,                          // ya que nos dira que no puede encontrar la ruta
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
+module.exports = nextConfig 
