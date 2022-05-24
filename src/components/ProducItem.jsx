706264e8575ab531d8872551a3c9c0741951cc5a
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-23 22:54:20 -0400

    hooks y lectura de api para las imagenes

--- src/components/ProducItem.jsx
+++ src/components/ProducItem.jsx
@@ -1,7 +1,8 @@
-import React, { useContext} from 'react';
-import '@styles/ProductItem.scss';
+import React, { useContext } from 'react';
 import AppContext from '@context/AppContext';
+import Image from 'next/image';
 import addToCartImage from '@icons/bt_add_to_cart.svg';
+import styles from'@styles/ProductItem.module.scss';
 
 const ProducItem = ({product}) => {
 	const { addToCart } = useContext(AppContext);
@@ -10,15 +11,15 @@ const ProducItem = ({product}) => {
 		addToCart(item);
 	}
     return (
-        <div className="ProductItem">
-			<img src= {product.images[0]} alt={product.title}/>
-			<div className="product-info">
-				<div>
+        <div className={styles.ProducItem}>
+			<Image loader={() => product.images[0]} src={product.images[0]} alt={product.title} width="100%" height="100%" layout="responsive" />
+			<div className={styles['product-info']}>
+		   		<div>
 					<p>$ {product.price}</p>
 					<p>{product.title}</p>
 				</div>
 				<figure onClick = {() => handleClick(product)} >
-					<img src={addToCartImage} alt="" />
+					<Image src={addToCartImage} alt="" />
 				</figure>
 			</div>
 		</div>
